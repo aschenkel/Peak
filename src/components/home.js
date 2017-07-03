8e2976eea5194f8956b7b29317fed26e169d6383
Axel Schenkelman <axel.schenkelman@gmail.com>
2017-07-03 20:37:20 -0300

    Add parallax component

--- src/components/home.js
+++ src/components/home.js
@@ -10,8 +10,9 @@ import {
 import { connect } from 'react-redux'
 import * as actions from '../store/actions/index'
 import FabricTwitterKit from 'react-native-fabric-twitterkit'
+import ParallaxScrollView from 'react-native-parallax-scrollview';
 
-const {width: windowWidth} = Dimensions.get('window');
+const {width: windowWidth,windowHeight } = Dimensions.get('window');
 
 
 class Home extends Component{
@@ -22,16 +23,29 @@ class Home extends Component{
     render(){
         return (
         //add loader structure
-        <View style={{flex: 1, marginTop:50}}>
-            <Text>Hey {this.props.user}!</Text>
-            <Text>Your name is {this.props.name}!</Text>
-            <Text>{this.props.imageURL}</Text>
-            <View style={styles.imageContainer}>
-                <Image source={{uri:""+this.props.imageURL}} 
-                style={styles.image}/>
-            </View>
-           
-        </View>)
+           <ParallaxScrollView
+              windowHeight={windowHeight}
+              backgroundSource={{uri:'https://i.imgur.com/ao1nxlD.png'}}
+              navBarTitle="Beak"
+              userName={this.props.name}
+              userTitle={"@"+this.props.user}
+              userImage={this.props.imageURL}
+              leftIcon={{name: 'logout', color: 'white', size: 22, type: 'simple-line-icon'}}
+              leftIconOnPress={() => this.setState({index: (this.state.index + 1 ) % 3})}
+              rightIcon={{name: 'pencil', color: 'white', size: 22, type: 'simple-line-icon'}}
+              rightIconOnPress={() => this.setState({index: (this.state.index + 1 ) % 3})}
+            >
+              <Text>Hello. I am sorry for raising a dead thread, but I have run into this exact problem, however the fix listed here does not apply (at least I don't think it does).
+
+            Here is my situation. The app has had this package installed and working fine for awhile now with only 1 custom iconFont for our company. Recently we have needed to branch out, so I added the FontAwesome font to the project. I followed all of the steps and everything works great if you run from xcode. I also completely removed the package and reinstalled using rnpm but got the same result. I have tried reseting npm cache, deleting node_modules folders and reinstalling everything, killing tempdir, and deleting xcode project derived data. I am out of options. Any help would be greatly appreciated!</Text>
+            <Text>Hello. I am sorry for raising a dead thread, but I have run into this exact problem, however the fix listed here does not apply (at least I don't think it does).
+
+            Here is my situation. The app has had this package installed and working fine for awhile now with only 1 custom iconFont for our company. Recently we have needed to branch out, so I added the FontAwesome font to the project. I followed all of the steps and everything works great if you run from xcode. I also completely removed the package and reinstalled using rnpm but got the same result. I have tried reseting npm cache, deleting node_modules folders and reinstalling everything, killing tempdir, and deleting xcode project derived data. I am out of options. Any help would be greatly appreciated!</Text>
+            <Text>Hello. I am sorry for raising a dead thread, but I have run into this exact problem, however the fix listed here does not apply (at least I don't think it does).
+
+            Here is my situation. The app has had this package installed and working fine for awhile now with only 1 custom iconFont for our company. Recently we have needed to branch out, so I added the FontAwesome font to the project. I followed all of the steps and everything works great if you run from xcode. I also completely removed the package and reinstalled using rnpm but got the same result. I have tried reseting npm cache, deleting node_modules folders and reinstalling everything, killing tempdir, and deleting xcode project derived data. I am out of options. Any help would be greatly appreciated!</Text>
+      </ParallaxScrollView>
+        )
     }
 
     fetchProfile() {
@@ -93,7 +107,6 @@ const mapStateToProps = (state) => {
         user: state.user.userName,
         userID: state.user.userID,
         name: state.user.name,
-        userName: state.user.userName,
         tweets: state.user.tweets,
         imageURL: correctURL
 }}
